6b99137e66722380e45ceb2029bab01389823b3e
romanklopyk <rx@lyc2.org>
2021-08-10 14:51:44 +0300

    js+

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
-console.log('Hello')
+console.log('Hello');
+
+
+$(document).on('click', '.menu-button', function() {
+    $('.menu-items').toggleClass('opened');
+    $('.middle').toggleClass('pad');
+    $('.bottom').toggleClass('pad');
+
+
+});
